39480d59355d0856ca7bbeec57881af77f188815
warrior-coder <gudkou2003@gmail.com>
2021-02-18 19:50:44 +0300

    Console update

--- Console-2048.cpp
+++ Console-2048.cpp
@@ -1,15 +1,33 @@
-﻿#include<iostream>
-#include<iomanip>
-#include<ctime>     // for srand
-#include<windows.h> // cls & sleep
-#include<conio.h>   // keys event
-#include<stdio.h>   // files io
+﻿#include <iostream>
+#include <iomanip>   // setw()
+#include <ctime>     // random values for srand
+#include <windows.h> // textcolor, sleep
+#include <conio.h>   // keys event
+#include <stdio.h>   // files io, system()
 
 using namespace std;
 
+enum ConsoleColor {
+    Black = 0,
+    Blue = 1,
+    Green = 2,
+    Cyan = 3,
+    Red = 4,
+    Magenta = 5,
+    Brown = 6,
+    LightGray = 7,
+    DarkGray = 8,
+    LightBlue = 9,
+    LightGreen = 10,
+    LightCyan = 11,
+    LightRed = 12,
+    LightMagenta = 13,
+    Yellow = 14,
+    White = 15
+};
 FILE* fl;
 const int boardSize = 4;
-int **board, SCORE, BEST, KE;
+int **board, SCORE, BEST, KeyEvent;
 bool GAME, WIN;
 
 void DrawFrames();
@@ -22,6 +40,9 @@ void TextColor(int, int);
 int main()
 {
     srand(time(NULL));
+    system("mode con cols=36 lines=14");
+    system("title 2048");
+
 
     board = new int* [boardSize];
     for (int i = 0; i < boardSize; i++)
@@ -41,21 +62,21 @@ int main()
         else
             BEST = 0;
         
-        KE = -1;
+        KeyEvent = -1;
         GAME = true;
         WIN = false;
         for (int i = 0; i < boardSize; i++)
             for (int j = 0; j < boardSize; j++)
                 board[i][j] = 0;
-        board[Random(0, 3)][Random(0, 3)] = Random(1,2) * 2;
+
+        board[Random(0, boardSize-1)][Random(0, boardSize-1)] = Random(1,2) * 2;
         
         DrawFrames();
         
         // GAME CYCLE
         while (GAME)
         {
-            KE = KeyDown();
-            if (KE > -1)
+            if (KeyDown() > -1)
             {
                 if (Logic())
                 {
@@ -77,20 +98,17 @@ int main()
         }
 
         // RESTART
-        char restart;
-        cout << endl << "R to restart "; cin >> restart;
-        if (restart == 'r' || restart == 'R')
-            GAME = true;
-        else
-            break;
+        cout << endl << "Space - restart" << endl << "Esc - exit ";
+        while (!GAME)
+            if (_kbhit())
+            {
+                switch (_getch())
+                {
+                    case 32: GAME = true; break;
+                    case 27: exit(0); break;
+                }
+            }
     }
-
-    for (int i = 0; i < boardSize; i++)
-        delete[] board[i];
-    delete[] board;
-    board = nullptr;
-
-    return 0;
 }
 
 int Random(int rmin, int rmax)
@@ -102,94 +120,85 @@ void DrawFrames()
 {
     system("cls");
 
-    TextColor(0, 13);
+    TextColor(Black, LightMagenta);
     cout << setw(12) << "2048" << endl;
-    TextColor(0, 15);
+    TextColor(Black, White);
 
     if (WIN)
     {
-        TextColor(0, 13);
-        cout << setw(16) << "YOU ARE WIN!" << endl;
-        TextColor(0, 15);
+        TextColor(Black, LightMagenta);
+        cout << setw(16) << "YOU ARE WIN!";
+        TextColor(Black, LightMagenta);
     }
-    else
-        cout << endl;
+    cout << endl;
 
     for (int i = 0; i < boardSize; i++)
     {
-        cout << "  ";
+        cout << "   ";
         for (int j = 0; j < boardSize; j++)
         {
             switch (board[i][j])
             {
                 case 0:
                     if ((i + j) % 2)
-                        TextColor(7, 7);
+                        TextColor(DarkGray, DarkGray);
                     else
-                        TextColor(8, 8);
+                        TextColor(LightGray, LightGray);
                     break;
-                case 2:  TextColor(14, 0); break;
-                case 4:  TextColor(6, 0); break;
-                case 8:  TextColor(12, 0); break;
-                case 16: TextColor(4, 0);  break;
-                case 32: TextColor(9, 0); break;
-                case 64: TextColor(1, 0); break;
-                case 128: TextColor(2, 0); break;
-                case 256: TextColor(10, 0); break;
-                case 512: TextColor(3, 0); break;
-                case 1024: TextColor(11, 0); break;
-                case 2048: TextColor(13, 0); break;
-                default: TextColor(0, 7); break;
+                case 2: TextColor(Yellow, Black); break;
+                case 4: TextColor(Brown, Black); break;
+                case 8: TextColor(LightRed, Black); break;
+                case 16: TextColor(Red, Black);  break;
+                case 32: TextColor(LightBlue, Black); break;
+                case 64: TextColor(Blue, Black); break;
+                case 128: TextColor(LightGreen, Black); break;
+                case 256: TextColor(Green, Black); break;
+                case 512: TextColor(LightCyan, Black); break;
+                case 1024: TextColor(Cyan, Black); break;
+                case 2048: TextColor(LightMagenta, Black); break;
+                default: TextColor(Magenta, Black); break;
             }
             cout << setw(4) << board[i][j];
         }
         
-        TextColor(0, 7);
+        TextColor(Black, White);
 
-        if (i == 1)
-        {
-            cout << "    SCORE: " << SCORE;
-        }
-        if (i == 2)
-        {
-            cout << "    BEST: " << BEST;
-        }
-        cout << endl;
+        if (i == 1) cout << "    SCORE: " << SCORE;
+        else if (i == 2) cout << "    BEST: " << BEST;
+        TextColor(Black, Black);
+        cout << "X" << endl;
+        TextColor(Black, White);
+
     }
-    
-    
 }
 
 int KeyDown()
 {
-    int KeyEvent = -1;
+    KeyEvent = -1;
     if (_kbhit())
         if (_getch() == 224)
             switch (_getch())
             {
-                case 72: KeyEvent = 3; break;
                 case 75: KeyEvent = 0; break;
-                case 77: KeyEvent = 2; break;
                 case 80: KeyEvent = 1; break;
+                case 77: KeyEvent = 2; break;
+                case 72: KeyEvent = 3; break;
             }
     return KeyEvent;
 }
 
 int Logic()
 {
-    for (int i = 0; i < KE; i++)
-        BoardRotate();
+    for (int i = 0; i < KeyEvent; i++) BoardRotate();
     
     for (int i = 0; i < boardSize; i++)
     {
-        for (int j = boardSize - 2; j > -1; j--)
+        for (int j = boardSize-2; j > -1; j--)
         {
             if (board[i][j] == 0 && board[i][j + 1] > 0)
             {
                 for (int l = j; l < boardSize; l++)
-                {
                     board[i][l] = board[i][l+1];
-                }
                 board[i][boardSize-1] = 0;
             }
         }
@@ -199,51 +208,36 @@ int Logic()
             if (board[i][j] == board[i][j+1])
             {
                 for (int l = j; l < boardSize-1; l++)
-                {
                     board[i][l] = board[i][l+1];
-                }
-                board[i][j] *= 2;
-                SCORE += board[i][j];
                 board[i][boardSize-1] = 0;
 
-                if (board[i][j] == 2048)
-                    WIN = true;
-                if (SCORE > BEST)
-                    BEST = SCORE;
+                SCORE += (board[i][j] *= 2);
+
+                if (board[i][j] >= 2048) WIN = true;
+                
             }
         }
     }
-
-    for (int i = KE; i < 4; i++)
-        BoardRotate();
+    if (SCORE > BEST) BEST = SCORE;
+    for (int i = KeyEvent; i < 4; i++) BoardRotate();
 
     int k = 0;
     for (int i = 0; i < boardSize; i++)
-    {
         for (int j = 0; j < boardSize; j++)
-        {
             if (board[i][j]) k++;
-        }
-        
-    }
-    if (k == 16)
-    {
-        return 1;
-    }
+    if (k == boardSize*boardSize) return 1;
 
     int ri, rj;
     while (true)
     {
-        ri = Random(0, 3);
-        rj = Random(0, 3);
+        ri = Random(0, boardSize-1);
+        rj = Random(0, boardSize-1);
         if (board[ri][rj] == 0)
         {
             board[ri][rj] = Random(1,2) * 2;
             return 0;
         }
     }
-    
-
 }
 
 void BoardRotate()
