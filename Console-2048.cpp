241f7078dcaa77ed83b2a12ac3f2de51b3e1ed20
warrior-coder <gudkou2003@gmail.com>
2021-06-15 14:42:04 +0300

    Fix gameover bug

--- Console-2048.cpp
+++ Console-2048.cpp
@@ -1,8 +1,8 @@
-﻿#include <iostream>
+﻿#include <iostream>  // cout
 #include <iomanip>   // setw()
-#include <ctime>     // random values for srand
-#include <windows.h> // textcolor, sleep
-#include <conio.h>   // keys event
+#include <ctime>     // random seed for srand
+#include <windows.h> // console color
+#include <conio.h>   // keydown event
 #include <stdio.h>   // files io, system()
 
 using namespace std;
@@ -25,14 +25,14 @@ enum ConsoleColor {
     Yellow = 14,
     White = 15
 };
-FILE* fl;
+FILE* fp;
 const int boardSize = 4;
 int **board, SCORE, BEST, KeyEvent;
 bool GAME, WIN;
 
 void DrawFrames();
 int KeyDown();
-int Logic();
+bool Logic();
 void BoardRotate();
 int Random(int, int);
 void TextColor(int, int);
@@ -40,40 +40,37 @@ void TextColor(int, int);
 int main()
 {
     srand(time(NULL));
-    system("mode con cols=36 lines=14");
+    system("mode con cols=40 lines=20");
     system("title 2048");
 
 
     board = new int* [boardSize];
-    for (int i = 0; i < boardSize; i++)
-        board[i] = new int[boardSize];
+    for (int i = 0; i < boardSize; i++) board[i] = new int[boardSize];
 
     while(true)
     {
-        // SETUP
+        // Setup
         SCORE = 0;
         
-        fopen_s(&fl, "record.dat", "r");
-        if (fl)
+        fopen_s(&fp, "record.dat", "rt");
+        if (fp)
         {
-            fscanf_s(fl, "%d", &BEST);
-            fclose(fl);
+            fscanf_s(fp, "%d", &BEST);
+            fclose(fp);
         }
-        else
-            BEST = 0;
+        else BEST = 0;
         
         KeyEvent = -1;
         GAME = true;
         WIN = false;
         for (int i = 0; i < boardSize; i++)
-            for (int j = 0; j < boardSize; j++)
-                board[i][j] = 0;
+            for (int j = 0; j < boardSize; j++) board[i][j] = 0;
 
         board[Random(0, boardSize-1)][Random(0, boardSize-1)] = Random(1,2) * 2;
         
         DrawFrames();
         
-        // GAME CYCLE
+        // Game cycle
         while (GAME)
         {
             if (KeyDown() > -1)
@@ -81,7 +78,8 @@ int main()
                 if (Logic())
                 {
                     TextColor(0, 13);
-                    cout << endl << setw(14) << "GAME OVER" << endl;
+                    cout << endl;
+                    cout << setw(14) << "GAME OVER" << endl;
                     TextColor(0, 15);
                     GAME = false;
                     break;
@@ -90,22 +88,29 @@ int main()
             }
         }
 
+        // Wright record
         if (BEST == SCORE)
         {
-            fopen_s(&fl, "record.dat", "wt");
-            fprintf(fl, "%d", BEST);
-            fclose(fl);
+            fopen_s(&fp, "record.dat", "wt");
+            if (fp)
+            {
+                fprintf(fp, "%d", BEST);
+            }
+            
         }
 
-        // RESTART
-        cout << endl << "Space - restart" << endl << "Esc - exit ";
+        // Restart
+        cout << endl;
+        cout << "Space - restart" << endl;
+        cout << "Esc - exit ";
+
         while (!GAME)
             if (_kbhit())
             {
                 switch (_getch())
                 {
                     case 32: GAME = true; break;
-                    case 27: exit(0); break;
+                    case 27: return 0;
                 }
             }
     }
@@ -132,6 +137,7 @@ void DrawFrames()
     }
     cout << endl;
 
+    // Color tiles
     for (int i = 0; i < boardSize; i++)
     {
         cout << "   ";
@@ -140,10 +146,8 @@ void DrawFrames()
             switch (board[i][j])
             {
                 case 0:
-                    if ((i + j) % 2)
-                        TextColor(DarkGray, DarkGray);
-                    else
-                        TextColor(LightGray, LightGray);
+                    if ((i + j) % 2) TextColor(DarkGray, DarkGray);
+                    else TextColor(LightGray, LightGray);
                     break;
                 case 2: TextColor(Yellow, Black); break;
                 case 4: TextColor(Brown, Black); break;
@@ -165,10 +169,10 @@ void DrawFrames()
 
         if (i == 1) cout << "    SCORE: " << SCORE;
         else if (i == 2) cout << "    BEST: " << BEST;
-        TextColor(Black, Black);
-        cout << "X" << endl;
-        TextColor(Black, White);
 
+        TextColor(Black, Black);
+        cout << " " << endl;
+        TextColor(Black, White);
     }
 }
 
@@ -187,18 +191,17 @@ int KeyDown()
     return KeyEvent;
 }
 
-int Logic()
+bool Logic()
 {
+    // Swipe
     for (int i = 0; i < KeyEvent; i++) BoardRotate();
-    
     for (int i = 0; i < boardSize; i++)
     {
         for (int j = boardSize-2; j > -1; j--)
         {
             if (board[i][j] == 0 && board[i][j + 1] > 0)
             {
-                for (int l = j; l < boardSize; l++)
-                    board[i][l] = board[i][l+1];
+                for (int l = j; l < boardSize; l++) board[i][l] = board[i][l+1];
                 board[i][boardSize-1] = 0;
             }
         }
@@ -207,26 +210,41 @@ int Logic()
         {
             if (board[i][j] == board[i][j+1])
             {
-                for (int l = j; l < boardSize-1; l++)
-                    board[i][l] = board[i][l+1];
+                for (int l = j; l < boardSize-1; l++) board[i][l] = board[i][l+1];
                 board[i][boardSize-1] = 0;
 
                 SCORE += (board[i][j] *= 2);
 
                 if (board[i][j] >= 2048) WIN = true;
-                
             }
         }
     }
-    if (SCORE > BEST) BEST = SCORE;
     for (int i = KeyEvent; i < 4; i++) BoardRotate();
 
+    // If record
+    if (SCORE > BEST) BEST = SCORE;
+
+    // If no free space
     int k = 0;
     for (int i = 0; i < boardSize; i++)
         for (int j = 0; j < boardSize; j++)
             if (board[i][j]) k++;
-    if (k == boardSize*boardSize) return 1;
+    if (k == boardSize * boardSize)
+    {
+        // If gemeover
+        k = 0;
+        for (int i = 0; i < boardSize; i++)
+            for (int j = 0; j < boardSize-1; j++)
+                if (board[i][j] == board[i][j+1]) k++;
 
+        for (int i = 0; i < boardSize-1; i++)
+            for (int j = 0; j < boardSize; j++)
+                if (board[i][j] == board[i+1][j]) k++;
+
+        return !k;
+    }
+
+    // Create new tile
     int ri, rj;
     while (true)
     {
@@ -259,8 +277,7 @@ void BoardRotate()
 		}
 }
 
-void TextColor(int bg, int txt)
+void TextColor(int bg_color, int text_color)
 {
-    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-    SetConsoleTextAttribute(hConsole, (bg << 4) | txt);
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), (bg_color << 4) | text_color);
 }
